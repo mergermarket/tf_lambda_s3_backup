c5cd70feea3b197a83896ac71ed397202d6b20ae
Hector Rivas Gandara <keymon@gmail.com>
2017-06-07 15:01:50 +0100

    Add some initial basic tests
    Add some initial based unit tests. Right now only check if terraform
    plan works.

---
+++ test/infra/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  skip_requesting_account_id  = true
+  max_retries                 = 1
+  access_key                  = "a"
+  secret_key                  = "a"
+  region                      = "eu-west-1"
+}
+
+module "lambda_s3_backup" {
+  source = "../.."
+
+  name                = "test"
+  bucket_name         = "test_bucket"
+  bind_host_path      = "/mnt/vol-1234"
+  bind_container_path = "/data"
+  cluster             = "test-cluster"
+  backup_env          = {
+    "VAR1" = "foo",
+    "VAR1" = "bar",
+  }
+}
