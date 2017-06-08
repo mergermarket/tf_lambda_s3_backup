44ae79835de98a9f7179dc74a19cd7518098ccc5
Hector Rivas Gandara <keymon@gmail.com>
2017-06-08 13:48:38 +0100

    Configure the scheduled lambda function
    Configure the lambda function, based on the code of [1]. It will
    start the code that runs the ECS task. It passes all the required
    variables
    
    Additionally the variables that are passed to the container, although
    it is not really required.
    
    The lambda function will have a role that allows:
         1. Write the logs in cloudwatch
         2. Start the ECS task and assume the role of the ECS task
    
    We also configure a cloudwatch even with a cron expression that would
    trigger the lambda function.
    
    Finally we get as parameters:
     - cluster name
     - command to run
     - cron expression

--- variables.tf
+++ variables.tf
@@ -7,6 +7,11 @@ variable "bucket_name" {
   default = "test" # FIXME
 }
 
+variable "lambda_cron_schedule" {
+  description = "The sceduling expression for how often the lambda function runs."
+  default = "rate(3 hours)"
+}
+
 variable "bind_host_path" {
   description = "Host volume to mount into the container. Must be set together with bind_host_path"
   default     = "/tmp/dummy"
@@ -17,9 +22,19 @@ variable "bind_container_path" {
   default     = "/tmp/dummy"
 }
 
+variable "cluster" {
+  description = "Name of the ECS cluster where the ECS task would run"
+  default     = "default"
+}
+
 variable "docker_image" {
   description = "Docker image to use for the task. It should contain all the logic to perform the dump and sync"
   default     = "mergermarket/docker-mysql-s3-backup"
+}
+
+variable "docker_command" {
+  description = "Custom command to run in the container"
+  default     = ""
 }
 
 variable "backup_env" {
